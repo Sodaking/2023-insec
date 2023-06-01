2ff54b35249dc181b17332301b77568fa0fd5d29
JinhwaJang <gdejin@gmail.com>
2023-06-01 20:57:30 +0900

    add certificate chain verification process. If signing cert and delivered(response) cert matched, it works.

--- client.py
+++ client.py
@@ -2,35 +2,67 @@ import socket
 import ssl
 from OpenSSL import crypto
 
-def verify_signature(public_key_file, signature, data):
-    with open(public_key_file, "rt") as f:
-        cert = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
+trusted_root_certificate = "rootCA"
+
+def load_certificate(certificate_file):
+    with open(certificate_file + ".crt", "rt") as f:
+        certificate = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
+    return crypto.dump_certificate(crypto.FILETYPE_PEM, certificate)
+
+def verify_signature(certificate, signature, data):
+    try:
+        cert = crypto.load_certificate(crypto.FILETYPE_PEM, certificate)
         public_key = cert.get_pubkey()
         pub_key_string = crypto.dump_publickey(crypto.FILETYPE_PEM, public_key)
-    try:
         return crypto.verify(cert, signature, data, "sha256") == None
     except crypto.Error:
         return False
 
-    
+def verify_certificate_chain(certificate, trusted_cert_pem):
+    # Load the certificate
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, certificate)
+
+    # Create and configure a X509Store
+    store = crypto.X509Store()
+
+    # Add trusted certs to the store
+    trusted_cert = crypto.load_certificate(crypto.FILETYPE_PEM, trusted_cert_pem)
+    store.add_cert(trusted_cert)
+
+    # Create a X509StoreContext with the cert and the store
+    store_ctx = crypto.X509StoreContext(store, cert)
+
+    # Verify the certificate, returns None if successful, raises exception otherwise
+    try:
+        store_ctx.verify_certificate()
+        print("Certificate chain is valid")
+        return True
+    except Exception as e:
+        print(f"Certificate chain Verification failed: {str(e)}")    
+        return False
+
 class TCPClient:
+    #def __init__(self, host='127.0.0.1', port=8080):
     def __init__(self, host='147.46.242.204', port=9990):
         self.host = host
         self.port = port
 
     def start(self):
         with socket.create_connection((self.host, self.port)) as sock:
+            root_certificate = load_certificate(trusted_root_certificate)
             domain = 'www.naver.com'
             sock.sendall(domain.encode())
             
-            data = sock.recv(1024)
-            signature, ip = data.split(b'separator')
-            # print(signature, ip)
-
-
-            is_valid = verify_signature("domain.crt", signature, ip)
+            data = sock.recv(4096)
+            ip, certificate, signature = data.split(b'separator')
+            certificate2 = certificate
+            is_valid = verify_certificate_chain(certificate, root_certificate)
+            if is_valid == False:
+                return
+    
+            is_valid = verify_signature(certificate2, signature, ip)
             if is_valid:
-                print(ip.decode())
+                print('Signature verification OK: ' + ip.decode())
             else:
                 print('The server is not legitimate!')
 
--- server.py
+++ server.py
@@ -2,12 +2,20 @@ import socket
 from OpenSSL import crypto
 import random
 
+pki_list = ["domain", "domain2", "bad"]
+
 def create_signature(private_key_file, data):
-    with open(private_key_file, "rt") as f:
+    with open(private_key_file + ".key", "rt") as f:
         private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read())
     return crypto.sign(private_key, data, "sha256")
 
+def load_certificate(certificate_file):
+    with open(certificate_file + ".crt", "rt") as f:
+        certificate = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
+    return crypto.dump_certificate(crypto.FILETYPE_PEM, certificate)
+
 class TCPServer:
+    #def __init__(self, host='127.0.0.1', port=8080):
     def __init__(self, host='147.46.242.204', port=9990):
         self.host = host
         self.port = port
@@ -23,11 +31,19 @@ class TCPServer:
                     data = newsocket.recv(1024)
                     ip = socket.gethostbyname(data).encode()
                     
-                    key = random.choice(["domain.key", "bad.key"])
-                    print(key)
-                    signature = create_signature(key, ip)
-                    # print(signature, ip)
-                    response = signature + b'separator' + ip
+                    #signing certificate choose
+                    pki_idx = random.choice(range(len(pki_list)))
+                    selected_pki = pki_list[pki_idx]
+                    print('Signing Certificate: ' + selected_pki)
+                    signature = create_signature(selected_pki, ip)
+
+                    #response certificate choose
+                    pki_idx = random.choice(range(len(pki_list)))
+                    selected_pki = pki_list[pki_idx]
+                    certificate = load_certificate(selected_pki)
+                    print('Delivered Certificate: ' + selected_pki)
+
+                    response = ip + b'separator' + certificate + b'separator' + signature
                     newsocket.sendall(response)
                 finally:
                     newsocket.close()
